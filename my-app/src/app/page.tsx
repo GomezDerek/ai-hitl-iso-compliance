aa36f5edc708157fbe14a12ed4129ae869f4ead6
Derek Gomez <demagome@ucsc.edu>
2024-11-02 16:06:03 -0700

    fixed linting errors for build

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -6,6 +6,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>ComPro</h1>
+        {/* <h2>built for the AI Tinkerer&apos;s Human-in-the-Loop Hackathon</h2> */}
         <h2>built for the AI Tinkerer's Human-in-the-Loop Hackathon</h2>
         <p>by Sahil, Jeff, & Derek</p>
       </main>
