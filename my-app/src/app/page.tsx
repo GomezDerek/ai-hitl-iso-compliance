72e32de47d706b84fcc0559f875c0001068ccca9
Derek Gomez <demagome@ucsc.edu>
2024-11-03 06:12:47 -0800

    rewrote copy and added hover effects for buttons

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -55,8 +55,9 @@ export default function Home() {
 
         <section id="right-panel">
           <h2>Is your business ISO compliant?</h2>
-          <p>Ensure your corpus files are in compliance with the provided ISO's</p>
-          <button className="primary">Check Compliance</button>
+          <h4>Ensure your corpus files are in compliance with ISO's!</h4>
+          <p>Get started by uploading your corpus files and copies of the ISO regulations you need to comply with</p>
+          <button className="primary" onClick={()=>alert("Summon the AI agents")}>Check Compliance</button>
         </section>
 
       </main>
