d5c3a2caca4ad0978f17a04828a99914e2fdd400
Derek Gomez <demagome@ucsc.edu>
2024-11-03 08:15:04 -0800

    fixed build errors

--- my-app/src/app/components/reasoningModal.tsx
+++ my-app/src/app/components/reasoningModal.tsx
@@ -1,11 +1,13 @@
 'use client'
 import React from 'react';
 
+// eslint-disable-next-line
 interface Props {
     reasoning: string,
     onClick: ()=>void
 }
 
+// eslint-disable-next-line
 export default function ReasoningModal(prop: string) {
     return (
         <>
