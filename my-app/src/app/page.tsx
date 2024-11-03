c6a3d2f0c80e07551a2b14f22a18624156a755e4
Derek Gomez <demagome@ucsc.edu>
2024-11-02 20:24:39 -0700

    baby frontend built

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,16 +1,56 @@
 // import Image from "next/image";
-import styles from "./page.module.css";
+import './page.css'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>ComPro</h1>
-        {/* <h2>built for the AI Tinkerer&apos;s Human-in-the-Loop Hackathon</h2> */}
-        <h2>built for the AI Tinkerer's Human-in-the-Loop Hackathon</h2>
-        <p>by Sahil, Jeff, & Derek</p>
+    // <div className={styles.page}>
+    <div className="page">
+      
+        <header>
+          <h1 id="title">ComPro</h1>
+          <h2>built for the AI Tinkerer's Human-in-the-Loop Hackathon</h2>
+          <p>by Sahil, Jeff, Andres, Derek</p>
+        </header>
+ 
+      {/* <main className={styles.main}> */}
+      <main>
+        
+        <section id="left-panel">
+          
+          <div className="file-section-header">
+            <p>ISO files</p>
+            <button className="primary">+</button>
+          </div>
+          <ul className="file-list">
+            <li>ISO_file_1.pdf</li>
+            <li>ISO_file_2.pdf</li>
+          </ul>
+
+          <div className="file-section-header">
+            <p>Corpus files</p>
+            <button className="primary">+</button>
+          </div>
+          <ul className="file-list">
+            <li>
+              Corpus_file_1.pdf 
+              {/* <input type="checkbox"></input>
+              <span className="checkmark"></span> */}
+            </li>
+            <li>Corpus_file_2.pdf</li>
+          </ul>
+        
+        </section>
+
+        <section id="right-panel">
+          <h2>Is your business ISO compliant?</h2>
+          <p>Ensure your selected corpus files are in compliance with the provided ISO's</p>
+          <button className="primary">Check Compliance</button>
+        </section>
+
       </main>
-      <footer className={styles.footer}>
+
+      {/* <footer className={styles.footer}> */}
+      <footer>
       </footer>
     </div>
   );
