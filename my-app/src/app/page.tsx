7591ec46a85e1281b96fa7a6bfaff979e159791a
Derek Gomez <demagome@ucsc.edu>
2024-11-03 05:37:52 -0800

    upload files UI

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,6 +2,7 @@
 import './page.css';
 import File from './file';
 import React, { useState } from 'react';
+import UploadModal from './uploadModal';
 
 export default function Home() {
   
@@ -11,10 +12,19 @@ export default function Home() {
   const[corpusFiles, setCorpusFiles] = useState<string[]>(["Corpus_file_1.pdf", "Corpus_file_2.pdf"]);
   // eslint-disable-next-line
   const[selectedCorpus, setSelectedCorpus] = useState<string[]>([])
+  const[isUploading, setIsUploading] = useState<boolean>(true);
+  const[uploadFileType, setUploadFileType] = useState<string>("");
+
+  function addFilesOnClick(fileType: string) {
+    setIsUploading(true);
+    setUploadFileType(fileType);
+  }
 
   return (
     // <div className={styles.page}>
     <div id="page">
+
+        { isUploading ? <UploadModal fileType={uploadFileType} onClickProp={()=>setIsUploading(false)}/> : null }
       
         <header>
           <h1 id="title">ComPro</h1>
@@ -22,14 +32,13 @@ export default function Home() {
           <p>by Sahil, Jeff, Andres, Derek</p>
         </header>
  
-      {/* <main className={styles.main}> */}
       <main>
         
         <section id="left-panel">
           
           <div className="file-section-header">
             <p>ISO files</p>
-            <button className="primary">+</button>
+            <button className="primary" onClick={()=>addFilesOnClick("ISO")}>+</button>
           </div>
           <ul className="file-list" id="iso-file-list">
             { isoFiles.map( (fileName, i) => <File name={fileName} key={i}/>) }
@@ -37,7 +46,7 @@ export default function Home() {
 
           <div className="file-section-header" id="corpus-files">
             <p>Corpus files</p>
-            <button className="primary">+</button>
+            <button className="primary" onClick={()=>addFilesOnClick("Corpus")}>+</button>
           </div>
           <ul className="file-list">
           { corpusFiles.map( (fileName, i) => <File name={fileName} key={i}/>) }
---
+++ my-app/src/app/uploadModal.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React, { useState } from 'react';
+
+interface UploadModalProps {
+    onClickProp: () => void;
+    fileType: string;
+}
+
+export default function UploadModal({ onClickProp, fileType }: UploadModalProps) {
+    return (
+        <>
+            <div className="opacity-screen" onClick={onClickProp}></div>
+            <div id="upload-modal">
+                <button className="x-button" onClick={onClickProp}>x</button>
+                <h2>{`Upload your ${fileType} files`}</h2>
+                {/* <p className="p2">We'll sort them for you</p> */}
+                {/* <label>
+                    ISO
+                    <input type="radio"></input>
+                </label>
+                <label>
+                    Corpus
+                    <input type="radio"></input>
+                </label> */}
+                <div id="upload-box" onClick={()=>alert("Trigger file explorer")}>
+                    <img src="upload-icon.svg" width="50px" height="50px"></img>
+                    <p className="p2">.pdf</p>
+                </div>
+            </div>
+        </>
+    )
+}
